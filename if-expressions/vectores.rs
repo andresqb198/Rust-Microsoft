88a8f7b8b82cd40532c9d6127bebffb443e5d5d5
andresqb198 <andresqb198@gmail.com>
2023-01-18 05:26:09 -0500

    Vectores and exercise

--- if-expressions/vectores.rs
+++ if-expressions/vectores.rs
@@ -1,8 +1,16 @@
 fn main(){
 
     // Declaracion de un vector inicializado con 3 valores
-    let three_nums = vec![15,3,46];
+    let mut three_nums = vec![15,3,46];
     println!("Initial vector: {:?}",three_nums);
+
+    // Indexacion de un vector
+    let three = three_nums[1];
+    println!("Vector: {:?}, three = {}",three_nums,three);
+
+    // Sumar 5 al valor del indice 1 que es 5 + 3 = 8
+    three_nums[1] = three_nums[1] + 5;
+    println!("Vector: {:?}",three_nums);
 
     // Declaracion de un vector inicializado con valores cero y tamano 5
     let zeroes = vec![0;5];
@@ -19,5 +27,7 @@ fn main(){
     println!("Fruits added {:?}",fruit);
 
     // Pop() elimina el valor del final del vector
+    println!("Pop off: {:?}",fruit.pop());
+    println!("Fruits: {:?}",fruit);
 
 }
